a98026f361df492c801feee32db425d79fdf43c3
Derik Badman <derik@springshare.com>
2026-01-14 15:14:44 -0500

    chore: run build

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,2 @@
-import('./bootstrap')
+import('./bootstrapLibChat')
 	.catch(err => console.error(err));
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ sharedMappings.register(
 module.exports = {
   context: path.resolve(__dirname), // Sets the context to the directory where webpack.config.js is
   output: {
-    uniqueName: "customModule",
+    uniqueName: "LibChat",
     publicPath: 'auto',
   },
   optimization: {
@@ -58,10 +58,10 @@ module.exports = {
         library: { type: "module" },
 
         // For remotes (please adjust)
-        name: "customModule",
+        name: "LibChat",
         filename: "remoteEntry.js",
         exposes: {
-            './custom-module': './src/bootstrap.ts',
+            './LibChat': './src/bootstrapLibChat.ts',
         },
 
         // For hosts (please adjust)
